5953969cf9091e5e1d75f3a504b7a5aa4b233997
jydnbach <jaydenkim95@gmail.com>
2024-06-14 14:40:22 +0900

    created redux store for react

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === 'decrement') {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+//create redux store
+const store = createStore(counterReducer); //point at reducer
+
+export default store;
